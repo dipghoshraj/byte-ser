5d83494dcbd66d219deb8396e290716762132c9a
dipghoshraj <dipghoshraj@gmail.com>
2026-07-11 23:16:00 +0530

    added derive details

---
+++ byteser_derive/src/lib.rs
@@ -0,0 +1,64 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
+
+#[proc_macro_derive(ByteSerializable)]
+pub fn derive_byte_serializable(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let fields = match input.data {
+        Data::Struct(s) => match s.fields {
+            Fields::Named(f) => f.named,
+            _ => panic!("Only named structs supported"),
+        },
+        _ => panic!("Only structs supported"),
+    };
+
+    let serialize_fields = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+
+        quote! {
+            <#ident as byteser::ByteSerializable>::byteSerialize(&self.#ident, out);
+        }
+    });
+
+    let deserialize_fields = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+
+        quote! {
+            #ident: <#ty as byteser::ByteSerializable>::byteDeserialize(input)?
+        }
+    });
+
+    let expanded = quote! {
+
+        impl byteser::ByteSerializable for #name {
+
+            fn byteSerialize(&self, out: &mut Vec<u8>) {
+
+                #(
+                    #serialize_fields
+                )*
+            }
+
+            fn byteDeserialize(input: &mut &[u8]) -> Result<Self,String> {
+
+                Ok(Self{
+
+                    #(
+                        #deserialize_fields,
+                    )*
+
+                })
+
+            }
+
+        }
+
+    };
+
+    TokenStream::from(expanded)
+}
